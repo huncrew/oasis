03837c5d664e6e69a23b30d94d89c855f24a304f
huncrew <mrdalegrant@gmail.com>
2024-06-30 10:45:44 +0100

    feat(messages): project restructure to sst added test stack and lambda function

---
+++ stacks/ApiStack.ts
@@ -0,0 +1,14 @@
+import { StackContext, Api } from "sst/constructs";
+
+export function API({ stack }: StackContext) {
+  const api = new Api(stack, "api", {
+    defaults: {},
+    routes: {
+      "GET /": "services/test-service/src/index.handler",
+    },
+  });
+
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+}
