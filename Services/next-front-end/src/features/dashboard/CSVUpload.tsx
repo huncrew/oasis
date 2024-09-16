7b3c8c412daae58e8fa0bba1fa5c5d3858675346
mac1 <mac1@mac1s-mbp.home>
2024-09-16 10:46:16 +0100

    feat(develop): csv stuff updated

--- Services/next-front-end/src/features/dashboard/CSVUpload.tsx
+++ Services/next-front-end/src/features/dashboard/CSVUpload.tsx
@@ -1,7 +1,6 @@
-"use client"; 
+"use client";
 import React, { useState } from 'react';
 import { useUser } from '@clerk/nextjs';
-
 import { v4 as uuidv4 } from 'uuid';
 
 const FileUpload = () => {
@@ -17,13 +16,9 @@ const FileUpload = () => {
   };
 
   const handleUpload = async () => {
-    console.log('called in handleUpload');
-    console.log(user);
     if (file && user) {
       const newJobId = uuidv4();
-      setJobId(newJobId); // Save the generated Job ID in the state
-
-      console.log(process.env.NEXT_PUBLIC_AWS_API_URL);
+      setJobId(newJobId);
 
       try {
         const response = await fetch(`${process.env.NEXT_PUBLIC_AWS_API_URL}/upload-csv`, {
@@ -32,7 +27,7 @@ const FileUpload = () => {
           headers: {
             'X-Job-Id': newJobId as string,
             'X-User-Id': user.id,
-            'Content-Type': 'text/csv',  // Set the correct content type
+            'Content-Type': 'text/csv',
           },
         });
 
@@ -48,12 +43,28 @@ const FileUpload = () => {
   };
 
   return (
-    <div className="mt-6">
-      <input type="file" onChange={handleFileChange} accept=".csv" />
-      <button onClick={handleUpload} className="btn-primary mt-2">
-        Upload CSV
-      </button>
-      {uploadStatus && <p className="mt-4">{uploadStatus}</p>}
+    <div className="bg-card p-5 rounded-md">
+      <h2 className="text-xl font-semibold text-primary">Upload Your CSV Data</h2>
+      <div className="mt-4">
+        <input
+          type="file"
+          onChange={handleFileChange}
+          accept=".csv"
+          className="block w-full text-sm text-muted-foreground
+            file:mr-4 file:py-2 file:px-4
+            file:rounded-md file:border-0
+            file:text-sm file:font-semibold
+            file:bg-primary file:text-primary-foreground
+            hover:file:bg-primary/80"
+        />
+        <button
+          onClick={handleUpload}
+          className="mt-4 px-4 py-2 bg-primary text-primary-foreground font-semibold rounded-md hover:bg-primary/80"
+        >
+          Upload CSV
+        </button>
+        {uploadStatus && <p className="mt-4 text-sm text-muted-foreground">{uploadStatus}</p>}
+      </div>
     </div>
   );
 };
