87b92f8bfd82eb1b9054f44221df2eda65708b01
huncrew <mrdalegrant@gmail.com>
2024-09-28 10:33:58 +0100

    feat(develop): middleware simplified with secret Key console shitttt

--- services/next-front-end/src/middleware.ts
+++ services/next-front-end/src/middleware.ts
@@ -35,7 +35,7 @@ export default function middleware(
         const authObj = auth();
 
 
-        console.log(process.env.CLERK_SECRET_KEY)
+        console.log('whats the secret key',process.env.CLERK_SECRET_KEY)
         console.log('authObj:', authObj);
 
         if (isProtectedRoute(req)) {
